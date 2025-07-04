1c15b5a1d577de9c5cacd1af20788cc10ae4910f
Duane Johnson <duane@schoolai.com>
2025-07-04 08:48:27 -0600

    Target es2020

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,12 +2,14 @@ import { defineConfig, type Options } from 'tsup'
 
 export default defineConfig(
   (cliOptions: Options): Options => ({
+    target: 'es2020',
+    treeshake: false,
     splitting: false,
     sourcemap: true,
     clean: true,
     dts: true, // Generate .d.ts files
     bundle: true, // Bundle Chevrotain and any other deps
-    format: ['esm', 'cjs'],
+    format: ['esm'],
     entry: ['src/index.ts'],
     // Allows overriding via tsup CLI flags
     ...cliOptions,
